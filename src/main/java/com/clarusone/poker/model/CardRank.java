7dd5aa4b64eb884df1c22cd4e41bc084774ba2a7
Swapan Mazumdar <swapan.mazumdar@gmail.com>
2020-03-19 18:16:27 +0000

    revised CardRank enum

--- src/main/java/com/clarusone/poker/model/CardRank.java
+++ src/main/java/com/clarusone/poker/model/CardRank.java
@@ -1,12 +1,12 @@
 package com.clarusone.poker.model;
 
 /**
- * Contains predefined constants of all known ranks in playing cards. Any new rank should be defined here for
- * extensibility.
+ * Contains predefined constants of all known ranks in playing cards in natural order. Any new rank should be defined
+ * here for extensibility. The lowest ordinal is 0 for TWO and the highest ordinal is 12 for ACE. Though the rank starts
+ * at 2 and goes up to 14. It will be straight forward to translate an input rank from 2 - 10 and map it with
+ * Enum<CardRank>. The challenge will be to map input ranks - A, K, Q, J.
  */
 public enum CardRank {
-
-    ACE(1),
     TWO(2),
     THREE(3),
     FOUR(4),
@@ -18,9 +18,40 @@ public enum CardRank {
     TEN(10),
     JACK(11),
     QUEEN(12),
-    KING(13);
+    KING(13),
+    ACE(14); // moved ACE to the highest ordinal position
 
     CardRank(int rank) {
     }
 
+    public static boolean isValid(int rank) {
+        boolean result = false; // assume it to be invalid to protect the system
+        switch (rank) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+                result = true;
+                break;
+            default:
+                result = false;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        CardRank[] cardRanks = CardRank.values();
+        for (CardRank cardRank : cardRanks) {
+            System.out.println(cardRank.name() + " " + cardRank.ordinal());
+        }
+    }
 }
